d316ef97d6f7a8c73a1fdf0a317479e15f13ce03
Chris <fanchi0917@gmail.com>
2018-03-31 11:41:29 +0800

    upload js
    將多餘註解掉的code刪除

--- Uniut 1/main.js
+++ Uniut 1/main.js
@@ -63,24 +63,3 @@ paperBtn.addEventListener('click', function(){
     document.getElementById("score-img").src = "https://media.giphy.com/media/QqkA9W8xEjKPC/giphy.gif";
   }
 });
-/*
-
-  } else if (choice === 2) {
-    alert('你出布');
-    if (pcChoice === 0) {
-      document.getElementById("pc").src = "scissors.png";
-      document.getElementById("user").src = "paper.png";
-      document.getElementById("score").innerHTML = "你輸了!";
-      document.getElementById("score-img").src = "https://media.giphy.com/media/11StaZ9Lj74oCY/giphy.gif";
-    }else if (pcChoice === 1) {
-      document.getElementById("pc").src = "stone.png";
-      document.getElementById("user").src = "paper.png";
-      document.getElementById("score").innerHTML = "你贏了!";
-      document.getElementById("score-img").src = "https://media.giphy.com/media/xMIlfwRRQNEcw/giphy.gif";
-    }else if (pcChoice === 2) {
-      document.getElementById("pc").src = "paper.png";
-      document.getElementById("user").src = "paper.png";
-      document.getElementById("score").innerHTML = "沒輸沒贏!";
-      document.getElementById("score-img").src = "https://media.giphy.com/media/QqkA9W8xEjKPC/giphy.gif";
-    }
-  }*/
