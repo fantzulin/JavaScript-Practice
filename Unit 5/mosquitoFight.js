24572dcc1b4c7f984f5effefae9714cfe04464a7
fantzulin <fanchi0917@gmail.com>
2019-04-20 21:06:50 +0800

    create tenbura every second

--- Unit 5/mosquitoFight.js
+++ Unit 5/mosquitoFight.js
@@ -3,13 +3,27 @@ var countdown = 10;
 
 var timer = setInterval(function () {
     var gameOver = false;
+
     if (document.getElementById('mosquito') === null && gameOver !== true) {
         var mosquito = document.createElement('img');
 
-        mosquito.src = 'images/tembura.png';
+        mosquito.src = 'images/tenbura-ori.png';
         mosquito.style.position = 'absolute';
 
-        var width = Math.floor(Math.random() * window.innerWidth);
-        console.log(width);
+        var x = Math.floor(Math.random() * window.innerWidth);
+        var y = Math.floor(Math.random() * window.innerHeight);
+
+        mosquito.style.left = x + 'px';
+        mosquito.style.top = y + 'px';
+        mosquito.style.width = '100px';
+        mosquito.id = 'mosquito';
+        document.body.appendChild(mosquito);
+
     }
-});
+}, 1000);
+
+var disappear = setInterval(function () {
+    if (document.getElementById('mosquito') !== null) {
+        document.body.removeChild(document.getElementById('mosquito'));
+    }
+}, 3000);
