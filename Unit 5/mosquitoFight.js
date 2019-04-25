4d2f16c53338b5ab49badfb3c86cf024fdb40fe1
fantzulin <fanchi0917@gmail.com>
2019-04-25 21:52:58 +0800

    fixed bug
    fixed width and height <0 bug, and timeover can't add score

--- Unit 5/mosquitoFight.js
+++ Unit 5/mosquitoFight.js
@@ -13,12 +13,21 @@ var timer = setInterval(function () {
         var x = Math.floor(Math.random() * window.innerWidth - 100);
         var y = Math.floor(Math.random() * window.innerHeight - 100);
 
+        if (x < 0) {
+            x = 10;
+
+            console.log('left:', x, 'top:', y);
+        } else if (y < 0) {
+            y = 10;
+            console.log('left:', x, 'top:', y);
+
+        }
+
         mosquito.style.left = x + 'px';
         mosquito.style.top = y + 'px';
         mosquito.style.width = '100px';
         mosquito.id = 'mosquito';
-        document.body.appendChild(mosquito);
-
+        document.getElementById('divBody').appendChild(mosquito);
     }
     countdown -= 1;
     document.getElementById('counter').innerHTML = countdown;
@@ -33,7 +42,7 @@ var timer = setInterval(function () {
 
 var disappear = setInterval(function () {
     if (document.getElementById('mosquito') !== null) {
-        document.body.removeChild(document.getElementById('mosquito'));
+        document.getElementById('divBody').removeChild(document.getElementById('mosquito'));
     }
 }, 2000);
 
@@ -43,7 +52,14 @@ var click = document.body.addEventListener('click', function (e) {
     if (e.target.id === 'mosquito') {
         console.log(e.target);
         e.target.src = 'images/tenbura.png';
-        score += 5;
+        if (countdown <= 0) {
+            score += 0;
+        } else {
+            score += 5;
+        }
+
         scoreBoard.innerHTML = score;
+
     }
+
 });
